cbdcb52310c0c1ad172385cda028a7288f8cbcd4
Rafael Dias Zendron <mmmarckos@gmail.com>
2020-04-02 06:49:26 -0300

    adicionada variavel port

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 requireDir("./src/models");
 
 app.use("/api", require("./src/routes"));
-
-app.listen(3001);
+const port = process.env.PORT || 3001;
+app.listen(port);
